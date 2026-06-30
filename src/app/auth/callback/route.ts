c693459a4a87d9e18f9440910fd85ee966b1e294
Arsham Fazel <arshamfazil.89.afn@gmail.com>
2026-06-30 20:58:01 +0330

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,7 +1,17 @@
-import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET() {
-  return NextResponse.redirect(
-    process.env.NEXT_PUBLIC_BASE_URL || "/"
-  );
+export async function GET(request: NextRequest) {
+  const code = request.nextUrl.searchParams.get("code")
+
+  if (code) {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  return NextResponse.redirect(new URL("/account", request.url))
 }
