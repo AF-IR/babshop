40d8a469f6d8964ab1201068496071eed98fc833
Arsham Fazel <arshamfazil.89.afn@gmail.com>
2026-06-30 11:18:34 +0330

    Update index.ts

--- src/lib/repositories/index.ts
+++ src/lib/repositories/index.ts
@@ -1,8 +1,7 @@
 // Re-export the JSON-backed repositories as the default implementations.
 // To swap backends (database, CMS, API), implement the same interfaces
 // and change these exports.
-
-export { jsonProductRepository as productRepository } from "./json-product-repository"
+export { supabaseProductRepository as productRepository } from "./supabase-product-repository";
 export { jsonCategoryRepository as categoryRepository } from "./json-category-repository"
 export { jsonBrandRepository as brandRepository } from "./json-brand-repository"
 export { jsonPageRepository as pageRepository } from "./json-page-repository"
