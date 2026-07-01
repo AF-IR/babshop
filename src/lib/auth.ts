58d55966e45d1a2974edfc92c289b93ec25db58d
Arsham Fazel <arshamfazil.89.afn@gmail.com>
2026-07-01 14:17:46 +0330

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,28 @@
 import { supabase } from "./supabase";
+export async function signIn(email: string, password: string) {
+  return supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+}
 
+export async function signUp(
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) {
+  return supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        firstName,
+        lastName,
+      },
+    },
+  });
+}
 export async function signInWithGoogle() {
   return supabase.auth.signInWithOAuth({
     provider: "google",
