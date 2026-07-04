7a83a661d6995b9b27c790a931ef5bd5fcef39b5
Arsham Fazel <arshamfazil.89.afn@gmail.com>
2026-07-04 17:05:08 +0330

    Create step-success.tsx

---
+++ src/components/checkout/step-success.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+export default function StepSuccess(){
+
+    return(
+
+        <div className="py-20 text-center">
+
+            <h2 className="text-2xl font-bold">
+
+                سفارش با موفقیت ثبت شد.
+
+            </h2>
+
+            <p className="mt-4 text-muted-foreground">
+
+                بعد از پرداخت به این صفحه هدایت خواهید شد.
+
+            </p>
+
+        </div>
+
+    )
+
+}
