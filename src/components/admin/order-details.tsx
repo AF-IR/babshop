baa4d92a13ff09e9d5446d39c6fd41c9c6479666
Arsham Fazel <arshamfazil.89.afn@gmail.com>
2026-07-08 16:07:52 +0330

    Create order-details.tsx

---
+++ src/components/admin/order-details.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+export default function OrderDetails(){
+
+return(
+
+<div className="space-y-6">
+
+<h1 className="text-3xl font-bold">
+
+جزئیات سفارش
+
+</h1>
+
+<div className="rounded-lg border p-8">
+
+در مرحله بعد اطلاعات سفارش از API خوانده خواهد شد.
+
+</div>
+
+</div>
+
+)
+
+}
