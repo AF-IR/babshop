62fe50e5ee7a5df4752519a9c756f030c8874b7f
Arsham Fazel <arshamfazil.89.afn@gmail.com>
2026-07-09 14:47:46 +0330

    Update layout.tsx

--- src/app/(store)/auth/layout.tsx
+++ src/app/(store)/auth/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
+  title: {
+    default: "حساب کاربری | ارزان پلاست",
+    template: "%s | ارزان پلاست",
+  },
+  description: "ورود، ثبت‌نام و مدیریت حساب کاربری در ارزان پلاست",
   robots: { index: false, follow: false },
 }
 
